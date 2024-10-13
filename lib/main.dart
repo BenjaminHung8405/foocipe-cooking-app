ff53a341a88626b60a05450ddbc75d0c8661dc57
KhanhRomVN <khanhromvn@gmail.com>
2024-10-13 19:54:11 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,3 @@ import 'app.dart';
 void main() {
   runApp(const MyApp());
 }
-
-void Fuckyou() {
-  print('Fuck you');
-}
-
-void Fuckyou3() {
-  print('Fuck you');
-}
-
-void FUckyou1() {
-  print('Fuck you');
-}
